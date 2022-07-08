eb2a258aa399206ead9b73ab09a79c0323e446cd
Peter Zhang <ultraswordkirbypink@gmail.com>
2022-07-08 13:12:40 -0400

    docs

--- main.py
+++ main.py
@@ -1,34 +1,61 @@
 import pygame
+from engine import clock, user_input, handler, animation
+
+from engine.window import Window
 from engine.filehandler import *
 
 
 WINDOW_CAPTION = "RPG Game"
 WINDOW_SIZE = [1280, 720]
+FB_SIZE = [360, 202]
 
 FPS = 30
 
-window = pygame.display.set_mode(WINDOW_SIZE, 0, 32)
-pygame.display.set_caption(WINDOW_CAPTION)
+Window.create_window(WINDOW_CAPTION, WINDOW_SIZE[0], WINDOW_SIZE[1], pygame.RESIZABLE, 16)
+# window.set_icon()
+fb = pygame.Surface(FB_SIZE, 0, 32).convert_alpha()
 
-clock = pygame.time.Clock()
+# --------- testing ----------- #
 
-
-image = Filehandler.get_image("assets/")
-
-
-
+animation.load_and_parse_aseprite_animation("assets/sprites/player.json")
+image = animation.Category.get_category("player").get_animation("idle").get_registry()
 
 
-running = True
+# ----------------------------- #
+clock.start()
+while Window.running:
+    fb.fill((255, 255, 255))
+    image.update()
+    fb.blit(image.get_frame(), (100, 100))
 
-while running:
-    window.fill((255, 255, 255))
+    # rescale framebuffer to window
+    Window.instance.blit(pygame.transform.scale(fb, (Window.WIDTH, Window.HEIGHT)), (0,0))
 
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
-            running = False
+            Window.running = False
+        elif e.type == pygame.KEYDOWN:
+            # keyboard press
+            user_input.key_press(e)
+        elif e.type == pygame.KEYUP:
+            # keyboard release
+            user_input.key_release(e)
+        elif e.type == pygame.MOUSEMOTION:
+            # mouse movement
+            user_input.mouse_move_update(e)
+        elif e.type == pygame.MOUSEBUTTONDOWN:
+            # mouse press
+            user_input.mouse_button_press(e)
+        elif e.type == pygame.MOUSEBUTTONUP:
+            # mouse release
+            user_input.mouse_button_release(e)
+        elif e.type == pygame.WINDOWRESIZED:
+            # window resized
+            Window.handle_resize(e)
+            fbsize = fb.get_size()
+            user_input.update_ratio(Window.WIDTH, Window.HEIGHT, fbsize[0], fbsize[1])
     pygame.display.flip()
-    clock.tick(FPS)
+    clock.update()
 
 
 pygame.quit()
--- test.py
+++ test.py
@@ -1,22 +1,3 @@
-import time
 
-
-buf = {'hi': 2, 2: 2}
-
-S = int(1e7)
-
-s = time.time()
-for i in range(S):
-    if 'hi' in buf:
-        pass
-print(time.time()-s)
-
-
-
-s=time.time()
-for i in range(S):
-    if buf.get('hi'):
-        pass
-print(time.time()-s)
 
 
